3d156e79c34ca1c5feffebd639cb62d47704cba4
Patricia <patriciastarck@gmail.com>
2023-08-23 10:28:01 -0300

    Atualização Patricia

--- insercaodedados.sql
+++ insercaodedados.sql
@@ -1,4 +1,4 @@
-select * from categoria;
+select * from endereco e ;
 
 insert into categoria (nome_categoria,descricao_categoria) values
 	('Raridades','Os itens mais raros'),
@@ -6,12 +6,13 @@ insert into categoria (nome_categoria,descricao_categoria) values
 	('Informática','Os Melhores itens de Informática'),
 	('Papelaria','Os Melhores itens de Papelaria'),
 	('Alimentos','Vários Tipos de Alimentos')
+;
 insert into endereco (pais,uf,cidade,cep,bairro,rua) values
-	('NOME DO PAÍS','NOME DA UF','NOME DA CIDADE','NÚMERO DO CEP','NOME DO BAIRRO','NOME DA RUA'),
-	('NOME DO PAÍS','NOME DA UF','NOME DA CIDADE','NÚMERO DO CEP','NOME DO BAIRRO','NOME DA RUA'),
-	('NOME DO PAÍS','NOME DA UF','NOME DA CIDADE','NÚMERO DO CEP','NOME DO BAIRRO','NOME DA RUA'),
-	('NOME DO PAÍS','NOME DA UF','NOME DA CIDADE','NÚMERO DO CEP','NOME DO BAIRRO','NOME DA RUA'),
-	('NOME DO PAÍS','NOME DA UF','NOME DA CIDADE','NÚMERO DO CEP','NOME DO BAIRRO','NOME DA RUA')	
+	('Brasil','RJ','Petrópolis','25665-001','Bingen','Paulo Hervê'),
+	('Brasil','MG','Tirandentes','25886-002','Castelo','Carlos da Gama'),
+	('Brasil','SP','São Paulo','25415-017','Sorriso','Prudente'),
+	('Brasil','RJ','Nova Friburgo','25684-025','Boa Vista','Prado Vasconcelos'),
+	('Estados Unidos','CA','Los Angeles','55698-000','Malibú','Orange Street')	
 ;
 insert into cliente (nome_completo,nome_de_usuario,email,cpf,data_de_nascimento,cod_endereco) values
 	('NOME DO CLIENTE','NOME DO USUÁRIO','EMAIL DO USUÁRIO','CPF DO USUÁRIO','DATA DE NASCIMENTO DO USUÁRIO','CÓDIGO DO ENDEREÇO DO USIÁRIO'),
@@ -27,3 +28,4 @@ insert into funcionario (nome_funcionario,cpf_funcionario) values
 	('NOME DO FUNCIONÁRIO','CPF DO FUNCIONÁRIO'),
 	('NOME DO FUNCIONÁRIO','CPF DO FUNCIONÁRIO')
 ;
+
--- tabelas.sql
+++ tabelas.sql
@@ -67,3 +67,4 @@ CREATE table funcionario_produto (
 );
 
 
+
